549e1971a3dcf06498debd563ea349572dcc9e72
andreas <asekkingstad@gmail.com>
2018-07-08 13:35:09 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,32 @@
+
+
+function processData(allText) {
+    allText = allText + "";
+    var record_num = 4;  // or however many elements there are in each row
+    var allTextLines = allText.split("\n");
+    var fjellLines = [];
+
+    for(var x in allTextLines) {
+        var y = allTextLines[x].split(";");
+
+        if(y[0].includes("Fjell")) {
+            console.log(y[3]);
+            fjellLines.push(y[3]);
+        }
+    }
+
+    document.getElementById("population").innerHTML = fjellLines[0];
+    // alert(lines);
+}
+
+
+
+$(document).ready(function() {
+        $.ajax({
+            'url' : "http://data.ssb.no/api/v0/dataset/1108.csv?lang=no",
+            'type' : 'GET',
+            'success' : function(data,status){
+                processData(data);
+        }
+    });
+});
